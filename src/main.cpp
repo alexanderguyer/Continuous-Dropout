ab3d133942cb1ae200403bc29ead737b397fcb29
Alexander Guyer <guyera@oregonstate.edu>
2020-03-02 08:22:47 -0800

    Check accuracy against training and test data after each epoch to measure overfitting

--- src/main.cpp
+++ src/main.cpp
@@ -37,6 +37,16 @@ void iterate(double alpha, double beta, int upper_bound, double training_rate, i
 	int batch_size = 50;
 	int num_reads = (num_sets / read_size) * read_size < num_sets ? (num_sets / read_size) + 1 : (num_sets / read_size);
 
+	// Open output file
+	ostringstream out_file_name;
+	out_file_name << fixed;
+	out_file_name << setprecision(1);
+	out_file_name << "data/result-a" << alpha << "-b" << beta << "-ub" << upper_bound << "-tr";
+	out_file_name << setprecision(5);
+	out_file_name << training_rate << ".txt";
+	ofstream out_file(out_file_name.str());
+	out_file << std::setprecision(5);
+
 	for (int epoch = 0; epoch < epochs; epoch++) {
 		if (epoch > 0) {
 			cout << ", " << flush;
@@ -63,70 +73,121 @@ void iterate(double alpha, double beta, int upper_bound, double training_rate, i
 			delete [] desired_outputs;
 		}
 
-		
-	}
+		// Check accuracy after each epoch.
+		// Shadow input variables with test data
+		// input variables
+		int num_sets = 10000;
+		int read_size = 1000;
+		int batch_size = 50;
+		int num_reads = (num_sets / read_size) * read_size < num_sets ? (num_sets / read_size) + 1 : (num_sets / read_size);
 
-	num_sets = 10000;
-	read_size = 1000;
-	batch_size = 50;
-	num_reads = (num_sets / read_size) * read_size < num_sets ? (num_sets / read_size) + 1 : (num_sets / read_size);
+		int num_correct = 0;
 
-	int num_correct = 0;
+		for(int i = 0; i < num_reads; i++){
+			int remaining = num_sets - i * read_size;
+			int specific_read_size = read_size < remaining ? read_size : remaining;
+			double **batch = itr.get_inputs(i * read_size, specific_read_size, mnist_iterator::TYPE_TEST);
+			double **desired_outputs = itr.get_desired_outputs(i * read_size, specific_read_size, mnist_iterator::TYPE_TEST);
 
-	for(int i = 0; i < num_reads; i++){
-		int remaining = num_sets - i * read_size;
-		int specific_read_size = read_size < remaining ? read_size : remaining;
-		double **batch = itr.get_inputs(i * read_size, specific_read_size, mnist_iterator::TYPE_TEST);
-		double **desired_outputs = itr.get_desired_outputs(i * read_size, specific_read_size, mnist_iterator::TYPE_TEST);
-
-		int num_batches = (specific_read_size / batch_size) * batch_size < specific_read_size ? (specific_read_size / batch_size) + 1 : (specific_read_size / batch_size);
-		for(int j = 0; j < num_batches; j++){
-			remaining = specific_read_size - j * batch_size;
-			double specific_batch_size = batch_size < remaining ? batch_size : remaining;
-			nn.calc_outputs(batch + j * batch_size, specific_batch_size);
-			for(int k = 0; k < specific_batch_size; k++){
-				int highest_predicted_index = 0, highest_actual_index = 0;
-				double highest_predicted = nn.get_output(k, 0), highest_actual = (desired_outputs + j * batch_size)[k][0];
-				for(int l = 1; l < 10; l++){
-					double output = nn.get_output(k, l);
-					if(output > highest_predicted){
-						highest_predicted_index = l;
-						highest_predicted = output;
+			int num_batches = (specific_read_size / batch_size) * batch_size < specific_read_size ? (specific_read_size / batch_size) + 1 : (specific_read_size / batch_size);
+			for(int j = 0; j < num_batches; j++){
+				remaining = specific_read_size - j * batch_size;
+				double specific_batch_size = batch_size < remaining ? batch_size : remaining;
+				nn.calc_outputs(batch + j * batch_size, specific_batch_size);
+				for(int k = 0; k < specific_batch_size; k++){
+					int highest_predicted_index = 0, highest_actual_index = 0;
+					double highest_predicted = nn.get_output(k, 0), highest_actual = (desired_outputs + j * batch_size)[k][0];
+					for(int l = 1; l < 10; l++){
+						double output = nn.get_output(k, l);
+						if(output > highest_predicted){
+							highest_predicted_index = l;
+							highest_predicted = output;
+						}
+						double desired_output = (desired_outputs + j * batch_size)[k][l];
+						if(desired_output > highest_actual){
+							highest_actual_index = l;
+							highest_actual = desired_output;
+						}
 					}
-					double desired_output = (desired_outputs + j * batch_size)[k][l];
-					if(desired_output > highest_actual){
-						highest_actual_index = l;
-						highest_actual = desired_output;
-					}
-				}
 
-				if(highest_predicted_index == highest_actual_index) {
-					num_correct++;
+					if(highest_predicted_index == highest_actual_index) {
+						num_correct++;
+					}
 				}
 			}
+
+			for(int j = 0; j < specific_read_size; j++){
+				delete [] batch[j];
+				delete [] desired_outputs[j];
+			}
+			delete [] batch;
+			delete [] desired_outputs;
 		}
 
-		for(int j = 0; j < specific_read_size; j++){
-			delete [] batch[j];
-			delete [] desired_outputs[j];
+		double percent_correct = (double) num_correct / 10000.0;
+		out_file << percent_correct << "/";
+
+		cout << setprecision(5) << percent_correct << "/" << flush;
+
+		// Also check accuracy against first 10,000 training data to
+		// measure overfitting:
+		// Check accuracy after each epoch.
+		// Shadow input variables with test data
+		// input variables
+		num_sets = 10000;
+		read_size = 1000;
+		batch_size = 50;
+		num_reads = (num_sets / read_size) * read_size < num_sets ? (num_sets / read_size) + 1 : (num_sets / read_size);
+
+		num_correct = 0;
+
+		for(int i = 0; i < num_reads; i++){
+			int remaining = num_sets - i * read_size;
+			int specific_read_size = read_size < remaining ? read_size : remaining;
+			double **batch = itr.get_inputs(i * read_size, specific_read_size, mnist_iterator::TYPE_TRAIN);
+			double **desired_outputs = itr.get_desired_outputs(i * read_size, specific_read_size, mnist_iterator::TYPE_TRAIN);
+
+			int num_batches = (specific_read_size / batch_size) * batch_size < specific_read_size ? (specific_read_size / batch_size) + 1 : (specific_read_size / batch_size);
+			for(int j = 0; j < num_batches; j++){
+				remaining = specific_read_size - j * batch_size;
+				double specific_batch_size = batch_size < remaining ? batch_size : remaining;
+				nn.calc_outputs(batch + j * batch_size, specific_batch_size);
+				for(int k = 0; k < specific_batch_size; k++){
+					int highest_predicted_index = 0, highest_actual_index = 0;
+					double highest_predicted = nn.get_output(k, 0), highest_actual = (desired_outputs + j * batch_size)[k][0];
+					for(int l = 1; l < 10; l++){
+						double output = nn.get_output(k, l);
+						if(output > highest_predicted){
+							highest_predicted_index = l;
+							highest_predicted = output;
+						}
+						double desired_output = (desired_outputs + j * batch_size)[k][l];
+						if(desired_output > highest_actual){
+							highest_actual_index = l;
+							highest_actual = desired_output;
+						}
+					}
+
+					if(highest_predicted_index == highest_actual_index) {
+						num_correct++;
+					}
+				}
+			}
+
+			for(int j = 0; j < specific_read_size; j++){
+				delete [] batch[j];
+				delete [] desired_outputs[j];
+			}
+			delete [] batch;
+			delete [] desired_outputs;
 		}
-		delete [] batch;
-		delete [] desired_outputs;
+
+		percent_correct = (double) num_correct / 10000.0;
+		out_file << percent_correct << "\n";
+
+		cout << setprecision(5) << percent_correct << flush;
 	}
-
-	ostringstream out_file_name;
-	out_file_name << fixed;
-	out_file_name << setprecision(1);
-	out_file_name << "data/result-a" << alpha << "-b" << beta << "-ub" << upper_bound << "-tr";
-	out_file_name << setprecision(5);
-	out_file_name << training_rate << ".txt";
-
-	ofstream out_file(out_file_name.str());
-	double percent_correct = (double) num_correct / 10000.0;
-	out_file << std::setprecision(5);
-	out_file << percent_correct << "\n";
-
-	cout << setprecision(5) << percent_correct << " => " << out_file_name.str() << "\n";
+	cout << " => " << out_file_name.str() << "\n";
 }
 
 int main(){
